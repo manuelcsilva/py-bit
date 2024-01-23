189609d20c38eae2230df1f1fe78ab7d6edd19d0
Manuel Silva <manuelcmsilva@hotmail.com>
2024-01-23 09:31:54 +0000

    add test02.py and setup.py

--- UI/pybit/main.py
+++ UI/pybit/main.py
@@ -1,9 +1,34 @@
-# This Python file uses the following encoding: utf-8
-import sys
-from PySide6.QtWidgets import QApplication
+import PySide6.QtCore
+import PySide6.QtGui
+import PySide6.QtWidgets
 
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    # ...
-    sys.exit(app.exec())
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(499, 378)
+        self.centralwidget = PySide6.QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.checkBox = PySide6.QCheckBox(self.centralwidget)
+        self.checkBox.setObjectName(u"checkBox")
+        self.checkBox.setGeometry(PySide6.QRect(40, 130, 76, 20))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = PySide6.QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(PySide6.QRect(0, 0, 499, 22))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = PySide6.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        PySide6.QMetaObject.connectSlotsByName(MainWindow)
+
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(PySide6.QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.checkBox.setText(PySide6.QCoreApplication.translate("MainWindow", u"CheckBox", None))
+    # retranslateUi
--- test01.py
+++ test01.py
@@ -8,27 +8,28 @@ class Ui_MainWindow(object):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
         MainWindow.resize(499, 378)
-        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget = PySide6.QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.checkBox = QCheckBox(self.centralwidget)
+        self.checkBox = PySide6.QCheckBox(self.centralwidget)
         self.checkBox.setObjectName(u"checkBox")
-        self.checkBox.setGeometry(QRect(40, 130, 76, 20))
+        self.checkBox.setGeometry(PySide6.QRect(40, 130, 76, 20))
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QMenuBar(MainWindow)
+        self.menubar = PySide6.QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 499, 22))
+        self.menubar.setGeometry(PySide6.QRect(0, 0, 499, 22))
         MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar = PySide6.QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
 
-        QMetaObject.connectSlotsByName(MainWindow)
+        PySide6.QMetaObject.connectSlotsByName(MainWindow)
 
     # setupUi
 
+
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.checkBox.setText(QCoreApplication.translate("MainWindow", u"CheckBox", None))
+        MainWindow.setWindowTitle(PySide6.QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.checkBox.setText(PySide6.QCoreApplication.translate("MainWindow", u"CheckBox", None))
     # retranslateUi
